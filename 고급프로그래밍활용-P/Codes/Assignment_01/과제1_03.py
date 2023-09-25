f6a30f77721625b3cb02bddd50826c15b6726c48
CANU <canu1832@gmail.com>
2023-09-25 20:59:27 +0900

    과제 제출

--- 고급프로그래밍활용-P/Codes/Assignment_01/과제1_03.py
+++ 고급프로그래밍활용-P/Codes/Assignment_01/과제1_03.py
@@ -1,7 +1,23 @@
-price_usd = float(input())
-discount_percent = float(input())
+original_price = float(input())
+discount_price = float(input())
 
-usdkrw= 1325.82
+discount_price_result = original_price - discount_price
 
-print(f"Price(dollar): {price_usd}, Discounted(dollar): {price_usd - discount_percent}")
-print(f"Price(won): {price_usd * usdkrw}, Discounted(won): {((price_usd - discount_percent)* usdkrw)}")
+print(original_price)
+print(discount_price_result)
+
+factor = 10 ** 2
+original_price_rounded = int(original_price * factor + 0.5) / factor
+discount_price_rounded = int((discount_price_result) * factor + 0.5) / factor
+
+print(f"Price(dollar): {original_price_rounded:.2f}, Discounted(dollar): {discount_price_rounded:.2f}")
+
+exchange_rate = 1325.82
+price_in_won = original_price * exchange_rate
+discounted_in_won = discount_price_result * exchange_rate
+
+factor = 1
+original_price_in_won = int(price_in_won * factor + 0.5) / factor
+discount_price_in_won = int((discounted_in_won) * factor + 0.5) / factor
+
+print(f"Price(won): {int(original_price_in_won)}, Discounted(won): {int(discount_price_in_won)}")
