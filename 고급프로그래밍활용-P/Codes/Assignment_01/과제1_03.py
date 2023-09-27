7ad6a59c60d71cf00a54ab219c8052e599c22780
C4NU <canu1832@gmail.com>
2023-09-27 17:57:32 +0900

    [ADD] 고프활 과제 1_3 해결

--- 고급프로그래밍활용-P/Codes/Assignment_01/과제1_03.py
+++ 고급프로그래밍활용-P/Codes/Assignment_01/과제1_03.py
@@ -1,23 +1,21 @@
-original_price = float(input())
-discount_price = float(input())
+price_dollar = float(input())
+discount_price_dollar = float(input())
 
-discount_price_result = original_price - discount_price
-
-print(original_price)
-print(discount_price_result)
+discounted_price_dollar = price_dollar - (price_dollar * discount_price_dollar / 100)
 
 factor = 10 ** 2
-original_price_rounded = int(original_price * factor + 0.5) / factor
-discount_price_rounded = int((discount_price_result) * factor + 0.5) / factor
+original_price_rounded = int(price_dollar * factor + 0.5) / factor
+discount_price_rounded = int((discounted_price_dollar) * factor + 0.5) / factor
 
 print(f"Price(dollar): {original_price_rounded:.2f}, Discounted(dollar): {discount_price_rounded:.2f}")
 
-exchange_rate = 1325.82
-price_in_won = original_price * exchange_rate
-discounted_in_won = discount_price_result * exchange_rate
+usdkrw = 1325.82
+
+price_won = price_dollar * usdkrw
+discounted_price_won = discounted_price_dollar * usdkrw
 
 factor = 1
-original_price_in_won = int(price_in_won * factor + 0.5) / factor
-discount_price_in_won = int((discounted_in_won) * factor + 0.5) / factor
+original_price_in_won = int(price_won * factor + 0.5) / factor
+discount_price_in_won = int((discounted_price_won) * factor + 0.5) / factor
 
 print(f"Price(won): {int(original_price_in_won)}, Discounted(won): {int(discount_price_in_won)}")
