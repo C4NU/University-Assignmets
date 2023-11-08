76d56e8e159bce6a3d84433cdf74c993c56736d4
C4NU <canu1832@gmail.com>
2023-11-08 17:53:33 +0900

    과제2 완료

--- 선형대수및프로그래밍/Assignment_02/main.py
+++ 선형대수및프로그래밍/Assignment_02/main.py
@@ -1,8 +1,5 @@
 # 18010714 전효재 과제 2
 
-def init_output_file(file_path):
-	f = open(file_path, 'w')
-	f.close()
 # 파일 경로 기반으로 읽어오는 read_file 함수
 def read_file(file_path):
 	# 반복문에서 파일 line 별 index로 활용하는 변수
@@ -64,53 +61,98 @@ def calc_determinant(matrix):
 	# matrix_size: 행렬 크기 (N, M) 사이즈
 	# matrix_size: 행렬 정보
 
+	matrix_data = matrix['data']
+	num_rows, num_cols = len(matrix_data), len(matrix_data[0])
+	
 	# 정방행렬이 아닐때
-	if matrix['size'][0] != matrix['size'][1]:
+	if num_rows != num_cols:
 		print("Unsupported Calculation: Not Square Matrix")
 		return None
 	
 	else:
 		# 행렬 크기가 1 by 1 일때
-		if matrix['size'][0] == matrix['size'][1] == 1:
-			return matrix['data'][0][0]
+		if num_rows == num_cols == 1:
+			return matrix_data[0][0]
 		
 		# 행렬 크기가 2 by 2 일때
-		elif matrix['size'][0] == matrix['size'][1] == 2:
-			return matrix['data'][0][0] * matrix['data'][1][1] - matrix['data'][1][0] * matrix['data'][0][1]
+		elif num_rows == num_cols == 2:
+			return matrix_data[0][0] * matrix_data[1][1] - matrix_data[0][1] * matrix_data[1][0]
 		
 		# 행렬 크기가 3 by 3 이상 일때
 		else:
 			det = 0
 
-			for col in range(matrix['size'][1]):
-				sub_matrix = []
+			for col in range(num_cols):
+				sub_matrix_data = []
 
-				for i in range(1, matrix['size'][0]):
-					sub_row = []
-					for j in range(matrix['size'][1]):
+				for i in range(1, num_rows):
+					sub_row_data = []
+					for j in range(num_cols):
 						if j != col:
-							sub_row.append(matrix['data'][i][j])
-					sub_matrix.append(sub_row)
-				sub_det = calc_determinant(sub_matrix)
+							sub_row_data.append(matrix_data[i][j])
+					sub_matrix_data.append(sub_row_data)
+				sub_matrix_dict = {"data": sub_matrix_data}
+				sub_det = calc_determinant(sub_matrix_dict)
 
 				if col % 2 == 0:
-					det += matrix[0][col] * sub_det
+					det += matrix_data[0][col] * sub_det
 				else:
-					det -= matrix[0][col] * sub_det
-			
-			return det
+					det -= matrix_data[0][col] * sub_det
+		return det
 
 def calc_cofactor_matrix(matrix):
 	# Cofactor Matrix 계산하는 함수
-	pass
+	matrix_data = matrix["data"]
+	num_rows, num_cols = len(matrix_data), len(matrix_data[0])
+	cofactor_data = []
 
-def print_matrix(matrix):
-	for i in range(0, matrix['size'][1]):
-		for j in range(0, matrix['size'][0]):
-			print(str(matrix['data'][i][j])+" ", end="")
-		print("")
+# 행렬 크기가 1 by 1 일때
+	if num_rows == num_cols == 1:
+		return matrix_data[0][0]
 	
-	print("")
+	else:
+		for i in range(num_rows):
+			cofactor_row = []
+			for j in range(num_cols):
+				sub_matrix_data = []
+				for row in range(num_rows):
+					if row != i:
+						sub_row = []
+						for col in range(num_cols):
+							if col != j:
+								sub_row.append(matrix_data[row][col])
+						sub_matrix_data.append(sub_row)
+				sub_matrix_dict = {"data": sub_matrix_data}
+				sub_det = calc_determinant(sub_matrix_dict)
+				cofactor_element = (-1) ** (i + j) * sub_det
+				cofactor_row.append(cofactor_element)
+			cofactor_data.append(cofactor_row)
+
+		return {"data": cofactor_data}
+
+def print_matrix(matrix, f, var):
+	# 입력받은 행렬을 출력할 때
+	if var == 0:	
+		f.write(str(matrix['size'][0]) + " " + str(matrix['size'][1]) + "\n")
+		for i in range(0, matrix['size'][1]):
+			for j in range(0, matrix['size'][0]):
+				f.write(str(matrix['data'][i][j])+" ")
+			f.write("\n")
+		
+		f.write("\n")
+	# Cofactor Matrix 연산 수행 후 출력할 때
+	if var == 1:
+		if type(matrix) is dict:
+			num_row, num_col = len(matrix['data'][0]), len(matrix['data'][1])
+
+			for i in range(0, num_col):
+				for j in range(0, num_row):
+					f.write(str(matrix['data'][i][j])+" ")
+				f.write("\n")
+
+			f.write("\n")
+		else:
+			f.write(str(matrix)+"\n\n")
 
 def main():
 	# 파일을 읽어올 경로와 파일명
@@ -120,17 +162,20 @@ def main():
 
 	index = 0
 	
-	init_output_file(output_file_path)
-
+	f = open((output_file_path), 'w')
+	
 	# 행렬 (dictionary)을 파일에서 읽어옴
 	matrices = read_file(file_path=input_file_path)
 	
 	while index < len(matrices):
-		#print_matrix(matrices[index])
-		determinant = calc_determinant(matrices[index])
-		cofactor_matrix = calc_cofactor_matrix(matrices[index])
+		print_matrix(matrices[index], f, 0)
 
-		print(f"det(행렬): {determinant}")
+		determinant = calc_determinant(matrices[index])
+		f.write(str(determinant)+"\n\n")
+
+		cofactor_matrix = calc_cofactor_matrix(matrices[index])
+		print_matrix(cofactor_matrix, f, 1)
+
 		index += 1
 
 # main으로 실행될 때 main 함수 실행
