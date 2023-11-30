addf21303cea92948c771cd16134222d3df51dc2
C4NU <canu1832@gmail.com>
2023-11-30 17:32:50 +0900

    데이터 엔지니어링 과제 추가

--- 고급프로그래밍활용-P/main.py
+++ 고급프로그래밍활용-P/main.py
@@ -1,40 +1,36 @@
-n = int(input())
+def main():
+    # 자연수 n 입력
+    n = int(input())
 
-n1 = int(input())
-n2 = int(input())
-n3 = int(input())
+    # 파일 이름 및 형식 입력
+    file_name = input()
 
-file_name = input()
+    # 단어 입력 및 파일에 저장
+    with open(file_name, 'w') as file:
+        for i in range(n):
+            word = input()
+            file.write(word + '\n')
 
-data = []
+    # 파일 불러와서 정렬
+    sorted_words = load_and_sort_words(file_name)
 
-for i in range(0, n):
-	name = input()
+    # 정렬된 결과 출력
+    print("\n정렬된 결과:")
+    for word in sorted_words:
+        print(word)
 
-	kor = input()
-	math = input()
-	eng = input()
-	
-	sum = str(int(kor) * (n1 / 100) + int(math) * (n2/100) + int(eng) * (n3/100))
+def load_and_sort_words(file_name):
+    # 파일 불러오기
+    with open(file_name, 'r') as file:
+        words = [line.strip() for line in file]
 
-	data.append([name, kor, math, eng, sum])
+    # 중복 제거를 위해 집합으로 변환 후 다시 리스트로 변환
+    unique_words = list(set(words))
 
-with open(file_name, 'w+') as file:
-	for num, i in enumerate(data):
-		if num+1 < len(data):
-			file.write(', '.join(i) + "\n")
-		else:
-			file
+    # 길이가 짧은 것부터 정렬, 길이가 같으면 사전 순으로 정렬
+    sorted_words = sorted(unique_words, key=lambda x: (len(x), x))
 
-temp = []
+    return sorted_words
 
-with open(file_name, "r") as file:
-	for fi in file:
-		ll = [ name.strip() for name in fi.split(",")]
-		temp.append(ll)
-
-temp.sort(key=lambda x:x[4])
-
-print(temp)
-for i in range(0, n):
-	print(f"{temp[i][0]} {float(temp[i][4]):.1f}")
+if __name__ == "__main__":
+    main()
